b16738a4414fbead3f449901ec9cb75e61f86fc5
Liu <18355530563@163.com>
2019-01-28 14:57:09 +0800

    Initial commit

---
+++ Example/FFCategoryKit/FFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FFAppDelegate.h
+//  FFCategoryKit
+//
+//  Created by LiuYimin on 01/28/2019.
+//  Copyright (c) 2019 LiuYimin. All rights reserved.
+//
+
+@import UIKit;
+
+@interface FFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
